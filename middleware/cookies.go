9b6a9ef5bc1c302ad64802969282bed40f787a19
SinclairPythonAkoto <sinclair.python@gmail.com>
2025-04-10 02:08:25 +0100

    cookies added to view & create reviews

--- middleware/cookies.go
+++ middleware/cookies.go
@@ -9,7 +9,7 @@ func ProfileDashboardSessionCookie(w http.ResponseWriter, sessionToken string, e
 	http.SetCookie(w, &http.Cookie{
 		Name:     "session_token",
 		Value:    sessionToken,
-		Expires:  expiryTime,
+		Expires:  expiryTime.Add(5 * time.Minute),
 		HttpOnly: true,
 		Path:     "/dashboard",
 		SameSite: http.SameSiteStrictMode,
@@ -21,9 +21,57 @@ func ProfileDashboardCSRFTokenCookie(w http.ResponseWriter, csrfToken string, ex
 	http.SetCookie(w, &http.Cookie{
 		Name:     "csrf_token",
 		Value:    csrfToken,
-		Expires:  expiryTime,
+		Expires:  expiryTime.Add(5 * time.Minute),
 		HttpOnly: false,
 		Path:     "/dashboard",
+		SameSite: http.SameSiteStrictMode,
+	})
+	return true
+}
+
+func CreateNewReviewSessionCookie(w http.ResponseWriter, sessionToken string, expiryTime time.Time) bool {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Expires:  expiryTime,
+		HttpOnly: true,
+		Path:     "/create-review",
+		SameSite: http.SameSiteStrictMode,
+	})
+	return true
+}
+
+func CreateNewReviewCSRFTokenCookie(w http.ResponseWriter, csrfToken string, expiryTime time.Time) bool {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    csrfToken,
+		Expires:  expiryTime,
+		HttpOnly: false,
+		Path:     "/create-review",
+		SameSite: http.SameSiteStrictMode,
+	})
+	return true
+}
+
+func ViewReviewSessionCookie(w http.ResponseWriter, sessionToken string, expiryTime time.Time) bool {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    sessionToken,
+		Expires:  expiryTime.Add(5 * time.Minute),
+		HttpOnly: true,
+		Path:     "/reviews",
+		SameSite: http.SameSiteStrictMode,
+	})
+	return true
+}
+
+func ViewReviewCSRFTokenCookie(w http.ResponseWriter, csrfToken string, expiryTime time.Time) bool {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    csrfToken,
+		Expires:  expiryTime.Add(5 * time.Minute),
+		HttpOnly: false,
+		Path:     "/reviews",
 		SameSite: http.SameSiteStrictMode,
 	})
 	return true
